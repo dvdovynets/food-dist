fd600aae1c82a0d6eacca4e078ad6437295d2fa3
Dmytro Vdovynets <dvdovynets@atlassian.com>
2022-05-13 15:13:07 +0300

    added modal

---
+++ js/script.js
@@ -0,0 +1,128 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+  //tabs
+  const tabs = document.querySelectorAll('.tabheader__item');
+  const tabsContent = document.querySelectorAll('.tabcontent');
+  const tabsParent = document.querySelector('.tabheader__items');
+
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target === item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+
+  //Timer
+  const deadline = '2022-05-14';
+
+  //returns the time left object
+  function getTimeRemaining(endtime) {
+    const timerEndpoint = Date.parse(endtime);
+    const timeNow = Date.now();
+    const total = (timerEndpoint >= timeNow) ? timerEndpoint - timeNow : 0;
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const seconds = Math.floor((total / 1000) % 60);
+
+    return {
+      'total': total,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  //adds 0 to a particular number
+  function getZero(number) {
+    if (number >= 0 && number < 10) {
+      return `0${number}`;
+    } else {
+      return number;
+    }
+  }
+
+  //parses clock on a page
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    //updates the clock according to an interval
+    function updateClock() {
+      const time = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(time.days);
+      hours.innerHTML = getZero(time.hours);
+      minutes.innerHTML = getZero(time.minutes);
+      seconds.innerHTML = getZero(time.seconds);
+
+      if (time.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadline);
+
+  //Modal window
+  const modalButtons = document.querySelectorAll('[data-modal]');
+  const closeButton = document.querySelector('[data-close]');
+  const modalWindow = document.querySelector('.modal');
+  
+  modalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      modalWindow.classList.add('show');
+      modalWindow.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+    });
+  });
+
+  closeButton.addEventListener('click', () => {
+      modalWindow.classList.add('hide');
+      modalWindow.classList.remove('show');
+      document.body.style.overflow = '';
+  });
+
+  modalWindow.addEventListener('click', (event) => {
+    if (event.target === modalWindow) {
+      modalWindow.classList.add('hide');
+      modalWindow.classList.remove('show');
+      document.body.style.overflow = '';
+    }
+  });
+
+});
